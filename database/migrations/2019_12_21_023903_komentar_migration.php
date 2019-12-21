f00c58a573412c7a94988983c153471e4ef8d5fd
dikimeip <pm159145@gmail.com>
2019-12-21 09:41:22 +0700

    membuat migration table komentar

---
+++ database/migrations/2019_12_21_023903_komentar_migration.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class KomentarMigration extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+         Schema::create('komentar', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('nama');
+            $table->string('email');
+            $table->string('keterangan');
+            $table->date('tanggal');
+            $table->string('status');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('about');
+    }
+}
