59c4fe576cd02c766ca1d762f8d2bfa7482e777d
dikimeip <pm159145@gmail.com>
2019-12-22 08:38:08 +0700

    membuat halaman dasboard admin

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -8,7 +8,7 @@ class LoginController extends Controller
 {
     public function index()
     {
-    	return view('login');
+    	return view('template.master');
     }
 
     public function do_login(Request $request)
