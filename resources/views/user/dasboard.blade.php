5baa03d20fec4e5dac224f6b76220feed818d2d3
dikimeip <pm159145@gmail.com>
2019-12-28 10:40:15 +0700

    membuat footer di halaman utama berita

--- resources/views/user/dasboard.blade.php
+++ resources/views/user/dasboard.blade.php
@@ -401,11 +401,45 @@
 					</div>
 				</div>
 			</div>
-
 		</div>
 	</div>
 </div>
 
+
+<nav class="navbar navbar-inverse navbar-button" style="margin-bottom: 0px;margin-top: 50px;color: white">
+	<div class="container">
+		<div class="row" style="margin-top: 40px;margin-bottom: 40px">
+			<div class="col-md-4">
+				<p class="text-center"><b>ABOUT DC NEWS</b></p>
+				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+				tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+				quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+				consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+				cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+				proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+			</div>
+			<div class="col-md-4">
+				<p class="text-center"><b>KONTAK DC NEWS</b></p>
+				<ul>
+					<li>Whatsapp : 085123123</li>
+					<li>Phone : 032123454</li>
+					<li>Email : dc_news@dc.com</li>
+					<li>Alamat : Dsn Mlati Ds Simongagrok Kec Dawarblandong Kab Mojokerto</li>
+				</ul>
+			</div>
+			<div class="col-md-4">
+				<p class="text-center"><b>KATEGORI DC NEWS</b></p>
+				<ul>
+					<li>EKONOMI</li>
+					<li>OLAHRAGA</li>
+					<li>POLITIK</li>
+					<li>TEKNOLOGI</li>
+				</ul>
+			</div>
+		</div>
+		<p class="text-center"><b>CREATED DC IT CORP </b></p>
+	</div> 
+</nav>
 <script src="{{asset('vendor/jquery-3.2.1.min.js')}} "></script>
  <script src="{{asset('vendor/bootstrap-4.1/popper.min.js')}} "></script>
  <script src="{{asset('vendor/bootstrap-4.1/bootstrap.min.js')}} "></script>
