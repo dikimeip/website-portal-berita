68161f65dd2fdec00902d54459965a50c891e982
dikimeip <pm159145@gmail.com>
2019-12-30 20:25:18 +0700

    membuat dan melakukan setting controller about

--- database/seeds/AboutSeeder.php
+++ database/seeds/AboutSeeder.php
@@ -12,7 +12,7 @@ class AboutSeeder extends Seeder
      */
     public function run()
     {
-        TentangModel::create({
+        TentangModel::create([
         	'tentang' => 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
 						tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
 						quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
@@ -23,7 +23,7 @@ class AboutSeeder extends Seeder
 			'alamat' => 'Dsn Mlati Ds Simongagrok Kec Dawarblandong Kab Mojokerto',
 			'email' => 'dc_news@dc.com',
 			'status' => 'aktif'
-        });
+        ]);
     }
 }
 
